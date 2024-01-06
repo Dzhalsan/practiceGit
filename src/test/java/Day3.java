5c332abf96a1e5386cbf0d19e103169d8d871930
Dzhalsan <erdniev08jalsan@gmail.com>
2024-01-05 19:49:10 -0500

    verison 0.2

--- src/test/java/Day3.java
+++ src/test/java/Day3.java
@@ -1,5 +1,7 @@
 public class Day3 {
     public static void main(String[] args) {
         System.out.println("This is version 0.1");
+        System.out.println("Added more cahnges.This is version 9,2");
+
     }
 }
